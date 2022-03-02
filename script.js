1f4b49137be981a5c23770b15455465074158c7f
Guillaume-G9 <ggonthier@live.fr>
2022-03-03 00:29:48 +0100

    added draw interface

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ cell.forEach(input => input.addEventListener('click', () => {
 
     if (player === 1 && input.textContent === '') {
         input.innerHTML = `X`;
+        document.querySelector(".circle-blue").style.display ="inline-block";
+        document.querySelector(".circle-orange").style.display ="none";
         player = 2;
     } else if (player === 2 && input.textContent === '') {
+        document.querySelector(".circle-blue").style.display ="none";
+        document.querySelector(".circle-orange").style.display ="inline-block";
         input.innerHTML = `O`;
         player = 1;
     }
+    // cell.forEach(cell => {
+    //     if (cell.innerHTML !== ``) {
+    //         document.querySelector(".message").style.display ="flex"
+    //         } 
+    // })
 }))
+
+function reset() {
+    cell.forEach(cell => {
+        cell.innerHTML = ``
+        player = 1;
+        document.querySelector(".circle-orange").style.display ="inline-block";
+        document.querySelector(".circle-blue").style.display ="none";
+        document.querySelector(".message").style.display ="none";
+    });
+}
